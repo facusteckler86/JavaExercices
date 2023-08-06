cab25b0884f2546e18843a6e8adaeacb3081fb2e
facusteckler86 <facundomsteckler@gmail.com>
2023-08-06 17:09:28 -0300

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a;
+        float b;
+        String s;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Ingrese un String");
+        s = in.nextLine();
+        System.out.println("Usted ingreso el string " + s);
+
+        System.out.println("Ingrese un integrador");
+        a = in.nextInt();
+        System.out.println("Tu integrador es " + a);
+
+        System.out.println("Ingrese un float");
+        b = in.nextFloat();
+        System.out.println("Usted ingreso un float  " + b);
     }
+
 }
+
+
